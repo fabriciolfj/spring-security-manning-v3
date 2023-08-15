cea68c6f7b39e938284f32fd13cd811bdbec0b7e
spark <fabricio.jacob@outlook.com>
2023-08-14 22:57:15 -0300

    add

--- csrf-service/src/main/java/com/github/csrfservice/HelloController.java
+++ csrf-service/src/main/java/com/github/csrfservice/HelloController.java
@@ -1,14 +1,20 @@
 package com.github.csrfservice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String getHello() {
+    public String getHello(HttpServletRequest request) {
+        CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+        log.info("CSRF token: "  + token.getToken());
         return "hello";
     }
 
